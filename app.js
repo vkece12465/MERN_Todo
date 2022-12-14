d37c19367bdf722cf083559544ae6d3da3e14d68
VINOD KUMAR M <34544927+vkece12465@users.noreply.github.com>
2022-12-14 18:49:59 +0000

    Routes and Database connected

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ require("dotenv").config;
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const auth = require("./middlewares/auth");
+const userRoute = require("./routes/user.route")
+const todoRoute = require("./routes/todo.route")
+const taskRoute = require("./routes/task.route")
+
+
+// Database
+const {dbConnection} = require("./config/dbConnection.config");
+dbConnection();
 
 // Middlewares
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+
+// Routers
+app.use('/', auth, userRoute)
+app.use('/', todoRoute)
+app.use('/', taskRoute)
+
+module.exports = app;
