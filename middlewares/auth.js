fe5e8679f71f50791bcc7abd0da4f6bf5ae50a0d
VINOD KUMAR M <34544927+vkece12465@users.noreply.github.com>
2022-12-14 18:25:20 +0000

    Middleware for auth created

---
+++ middlewares/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("./../models/user.model")
+const {SECRET_TOKEN} = process.env;
+
+
+const auth = (req, res, next) => {
+    console.log(req.cookies);
+    const {token} = req.cookies;
+
+    // pure token
+
+    /*
+    Authorization: "Bearer long token"
+    const token= req.header("Authorization").replace("bearer ", "") */
+
+    // If no token
+    if(!token){
+        return res.status(403).send("access denied");
+    }
+
+    // Verify token
+    try {
+        const decode = jwt.verify(token, SECRET_TOKEN)
+        console.log(decode)
+        req.user = decode
+
+    } catch (error) {
+        return res.status(403).send("Invalid token");
+
+    }
+    return next()
+}
+
+module.exports = auth;
