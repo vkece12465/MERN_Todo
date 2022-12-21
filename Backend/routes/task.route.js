f5b30f1287bedd85464ebd0f0aa12a9052c9b4cb
VINOD KUMAR M <34544927+vkece12465@users.noreply.github.com>
2022-12-21 17:18:14 +0000

    Modified in exports

--- Backend/routes/task.route.js
+++ Backend/routes/task.route.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const {Home, createTask, getTask, updateTask, DeleteTask} = require("./../controllers/task.controller");
 
 router.get('/', Home);
 router.post('/todo/createTask', createTask);
-router.get('/todo/getTask', getTask);
-router.put('/todo/updateTask/:_id', updateTask);
-router.delete('/todo/DeleteTask/:_id', DeleteTask);
+router.get('/todo/getTask/:id', getTask);
+router.put('/todo/updateTask/:id', updateTask);
+router.delete('/todo/DeleteTask/:id', DeleteTask);
 
-export default router;
+module.exports = router;
